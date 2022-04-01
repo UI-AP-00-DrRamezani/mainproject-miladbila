bda4d768bbd73458631936c19aa17c78c16abb3b
miladbila <btavakoli14@gmail.com>
2022-04-01 22:24:10 +0430

    add products attributes

--- shop/entities/Category.java
+++ shop/entities/Category.java
@@ -1,7 +1,10 @@
 package shop.entities;
 
+import java.util.ArrayList;
+
 public class Category {
     private String name;
     private String attributes;
+    private ArrayList<Product> products=new ArrayList<>();
 
 }
