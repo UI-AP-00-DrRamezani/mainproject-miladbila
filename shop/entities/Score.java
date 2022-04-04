dbfadd9fd60f166f9049ef3d175f6cbf11d2dac0
miladbila <btavakoli14@gmail.com>
2022-04-04 12:12:19 +0430

    changing user attribute type from String to Buyer

--- shop/entities/Score.java
+++ shop/entities/Score.java
@@ -1,17 +1,19 @@
 package shop.entities;
 
+import shop.roles.Buyer;
+
 public class Score {
-    private final String user;
+    private final Buyer user;
     private double score;
     private final String product;
 
-    public Score(String user, double score, String product) {
+    public Score(Buyer user, double score, String product) {
         this.user = user;
         this.score = score;
         this.product = product;
     }
 
-    public String getUser() {
+    public Buyer getUser() {
         return user;
     }
 
