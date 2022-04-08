008b7eed08f4c50150e87b4b6cfd7bca2be507d1
miladbila <btavakoli14@gmail.com>
2022-04-08 16:43:20 +0430

    adding selection menu to main page

--- shop/pages/Main.java
+++ shop/pages/Main.java
@@ -1,7 +1,31 @@
 package shop.pages;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Welcome to Online Shop\n\n");
+            System.out.println("1. UserArea");
+            System.out.println("2. View products");
+            System.out.println("3. Exit");
+            Scanner input = new Scanner(System.in);
+            int selectedNumber = input.nextInt();
+            input.nextLine();
+            switch (selectedNumber) {
+                case 1:
+                    UserArea.userArea();
+                    break;
+                case 2:
+                    ShowProductList.selectCategory(null);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Please select the correct option");
+            }
+        }
     }
 }
