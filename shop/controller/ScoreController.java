ac1c7a2b5ed97b78cc6c306bb6d0491b3e5ab6a1
miladbila <btavakoli14@gmail.com>
2022-04-10 19:43:13 +0430

    adding score controller class and generateAverage method

---
+++ shop/controller/ScoreController.java
@@ -0,0 +1,29 @@
+package shop.controller;
+
+import shop.entities.Score;
+import shop.entities.products.Product;
+
+import java.util.ArrayList;
+
+public class ScoreController {
+    private static ArrayList<Score> scores = new ArrayList<>();
+
+    public static ArrayList<Score> getScores() {
+        return scores;
+    }
+
+    public static void setScore(Score score) {
+        scores.add(score);
+    }
+
+    public static void generateAverage(Product product) {
+        double sum = 0;
+        int counter = 0;
+        for (Score a : scores)
+            if (a.getProduct() == product) {
+                counter++;
+                sum += a.getScore();
+            }
+        product.setAverageScoreOfBuyers(sum/counter);
+    }
+}
